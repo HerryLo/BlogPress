907c705d825f4a90fbc7b59928951b8f79bbf6bb
herrylo <herryloyopai@163.com>
2019-08-14 00:06:58 +0800

    set sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -21,9 +21,9 @@ git commit -m 'deploy'
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-git config --local user.name "Herrylo"
+git config --local user.name "HerryLo"
 git config --local user.email "herryloyopai@163.com"
 
-git push -f https://${access_token}@github.com/HerryLo/HerryLo.github.io.git master
+git push -f https://${access_token}@github.com/HerryLo/HerryLo.github.io.git master:gh-pages
 
 cd -
