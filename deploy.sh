a0a8f5a4d2a7d16e37467c3233914e8d249b63fc
herrylo <herryloyopai@163.com>
2019-08-22 15:10:31 +0800

    修改deploy

--- deploy.sh
+++ deploy.sh
@@ -2,18 +2,20 @@
 
 # 确保脚本抛出遇到的错误
 set -e
+basepath=$(cd `dirname $0`; pwd)
+sourcePath=$basepath'/docs/.vuepress/dist/'
 
+echo "开始构建项目"
 # 生成静态文件
 npm run build
-
-# 进入生成的文件夹
-cd docs/.vuepress/dist
+echo "构建完毕"
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
+
 
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:HerryLo/HerryLo.github.io.git master
@@ -23,6 +25,8 @@ git commit -m 'deploy'
 git config --local user.name "HerryLo"
 git config --local user.email "herryloyopai@163.com"
 
+echo "开始上传-----------"
 git push -f git@github.com:HerryLo/HerryLo.github.io.git master
 
+echo "上传完毕"
 cd -
