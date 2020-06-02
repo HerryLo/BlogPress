5d163997db24c90dfa2e3094b4470a5845828152
HerryLo <herryloyopai@163.com>
2020-06-02 23:31:58 +0800

    set imgList

---
+++ docs/.vuepress/components/IndeximgcardComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="imgCard">
+        <a  
+            class="img"
+            v-for="item in list"
+            v-bind:href="item.href"
+            v-bind:key="item.href"
+            target="_blank">
+            <img v-bind:src="item.imgUrl" />
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'IndeximgcardComponent',
+    data() {
+        return {
+            list: [
+                {
+                    "href": "https://www.kuaikanmanhua.com/web/topic/2583/","imgUrl": "/bcmw2-qiw8c-2.jpg"
+                },{
+                    "href": "https://www.manhuabei.com/manhua/guimiezhiren/","imgUrl": "/gmzr.jpg"
+                }
+            ]
+        }
+    },
+    mounted() {
+
+    }
+}
+</script>
+
+<style lang="stylus">
+    .imgCard {
+        display: flex;
+        align-items: center;
+        justify-content space-around;
+        .img {
+            width 360px;
+            border-radius 8px;
+        }
+    }
+</style>
