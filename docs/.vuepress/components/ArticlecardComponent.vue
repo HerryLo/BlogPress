753233f1a92a2b207359e44dc1f76025b3c30521
HerryLo <herryloyopai@163.com>
2022-11-19 21:34:45 +0800

    set front页面样式

--- docs/.vuepress/components/ArticlecardComponent.vue
+++ docs/.vuepress/components/ArticlecardComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ArtCard">
     <div class="cardLists" v-for="(item, index) in list" v-bind:key="item.path">
-      <div class="createtime">{{ item.frontmatter.createDate }} </div>
+      <div class="createtime">{{ item.frontmatter.createDate }}</div>
       <a class="itemCard" v-bind:href="item.path">
         <div class="title" v-bind:href="item.path">
           <div>{{ item.frontmatter.title }}</div>
@@ -9,7 +9,7 @@
         <div class="desc">
           <!-- <span v-for="slug in item.headers" v-bind:key="slug">{{slug.slug}}</span> -->
         </div>
-        <div class="line-height" v-if="index !== list.length-1"></div>
+        <div class="line-height" v-if="index !== list.length - 1"></div>
         <!-- <div class="detail tags">
           <div
             class="tag"
@@ -84,87 +84,117 @@ export default {
 </script>
 
 <style lang="stylus">
+.ArtCard {
+  margin-left: 50px;
+}
+
 .cardLists {
   display: flex;
   align-items: center;
+  justify-content: flex-start;
   position: relative;
 }
+
 .createtime {
   color: #3eaf7c;
   font-size: 13px;
   display: flex;
   align-items: center;
-  cursor pointer
-  margin-right: 10px;
+  cursor: pointer;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  padding-right: 10px;
 }
+
 .createtime::before {
   content: '🍅';
   display: inline-block;
   border-radius: 50%;
   margin-right: 4px;
-  cursor pointer
+  cursor: pointer;
 }
+
 .createtime::after {
   content: '👉';
-    display: inline-block;
-    cursor: pointer;
-    position: relative;
-    top: -3px;
-    left: 5px;
+  display: inline-block;
+  cursor: pointer;
+  position: relative;
+  top: -3px;
+  left: 5px;
 }
+
 .line-height {
-  background: #3eaf7c;
   border-radius: 2px;
-  height: 50px;
+  height: 51px;
   width: 1.5px;
   position: absolute;
-  top: 37px;
+  top: 38px;
   left: 8px;
+  background-color: #3eaf7c;
 }
+
 .itemCard {
-    flex: 1 1 auto;
-    padding: 10px 0px;
-    padding-left: 20px;
-    border-radius: 10px;
-    margin: 8px 5px;
-    box-shadow: 0px 0px 5px 1px #eee;
-    display block;
+  padding: 10px 10px;
+  padding-left: 20px;
+  border-radius: 10px;
+  margin: 8px 5px;
+  box-shadow: 0px 0px 5px 1px #eee;
+  display: block;
 
-    .title {
-        font-weight 700
-        color #000
-        font-size 18px
-    }
+  .title {
+    font-weight: 700;
+    color: #000;
+    font-size: 18px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
 
-    .desc {
-        color #eee
-    }
+  .desc {
+    color: #eee;
+  }
 
-    .detail {
-        color: #aaa;
-        margin-top: 10px;
-        font-size: 14px;
-    }
+  .detail {
+    color: #aaa;
+    margin-top: 10px;
+    font-size: 14px;
+  }
 
-    .tags {
-        color #ec4646;
-        display: flex;
-        align-items center
-        .tag  {
-            border-radius: 10px;
-            border: 1px solid;
-            padding: 2px 5px;
-            font-weight 900
-            font-size 12px
-            margin-right 5px
-        }
+  .tags {
+    color: #ec4646;
+    display: flex;
+    align-items: center;
+
+    .tag {
+      border-radius: 10px;
+      border: 1px solid;
+      padding: 2px 5px;
+      font-weight: 900;
+      font-size: 12px;
+      margin-right: 5px;
     }
+  }
 }
 
 .itemCard:hover {
-    background: rgba(215, 232, 224, 1)!important;
+  background: rgba(215, 232, 224, 1) !important;
 }
-.itemCard:nth-child(n){
-    background: rgba(215, 232, 224, 0.5);
+
+.itemCard:nth-child(n) {
+  background: rgba(215, 232, 224, 0.5);
 }
+
+@media (max-width: 1300px) {
+  .ArtCard {
+    margin-left: 0px;
+  }
+}
+@media (max-width: 1111px) {
+  .createtime,
+  .line-height {
+    display: none;
+  }
+}
+
 </style>
