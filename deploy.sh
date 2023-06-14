6cd06f849842790ef4eb207f6a29cb0b58ee24e1
HerryLo <herryloyopai@163.com>
2023-06-14 11:29:33 +0800

    add 修改deploy

--- deploy.sh
+++ deploy.sh
@@ -9,35 +9,12 @@ set -e
 basepath=$(cd `dirname $0`; pwd)
 sourcePath=$basepath'/docs/.vuepress/dist/'
 
-# 遍历html 插入友盟
-function fileForEach() {
-    for file in `ls $1`
-    do 
-        if [ -d $1"/"$file ]
-        then
-            if [ -n "`find $1$file -maxdepth 1 -name '*.html'`" ];
-            then
-                echo $1$file"/"*.html
-                sed -i '/</head>\</head><script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "https://hm.baidu.com/hm.js?03fcbc1674d5d46ad9125d7e0dbb5d51";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>' $1$file"/"*.html
-            fi
-        fi
-    done
-}
-
 echo "开始构建项目"
 # 生成静态文件
 npm run build
 echo "构建完毕"
 
-fileForEach $sourcePath
-
 cd $sourcePath
-
-# echo "添加友盟代码"
-# # ergodic $sourcePath
-# sed -i '' '/<\/body/a\ 
-# <script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1277950578&web_id=1277950578"></script><style>body>a{display:none!important;}</style>' $sourcePath'/index.html'
-# echo "添加代码执行完毕"
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
